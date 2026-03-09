82a47c2c977dcbc72e68b75f0c6727bd9a0eb5b2
Wenhao Wu <ww247@duke.edu>
2026-03-09 14:42:54 -0400

    "try"

--- baselines/SVD-LLM/flashsvd_component/svd_llama.py
+++ baselines/SVD-LLM/flashsvd_component/svd_llama.py
@@ -2657,7 +2657,7 @@ class SVD_LlamaAttention(nn.Module):
             attn_output = self.o_u_proj(self.o_v_proj(attn_output))
             if not output_attentions:
                 attn_weights = None
-            return attn_output, attn_weights
+            return attn_output, attn_weights, None
 
         # Build low-rank P factors: [B, M, R] -> expand to [B, H, M, R]
         Pq = self.q_v_proj(hidden_states)  # [B, M, R]
@@ -2781,5 +2781,5 @@ class SVD_LlamaAttention(nn.Module):
         attn_output = self.o_u_proj(self.o_v_proj(attn_output))
 
         attn_weights = None
-        return attn_output, attn_weights
+        return attn_output, attn_weights, None
     
